0cbbd8c8766fdb1f7b612088bbb4cb10d3d13b28
gytjd <hys339631@gmail.com>
2024-10-30 16:31:27 +0900

    #Fix: Application 이름 변경

--- src/main/java/AIWA/MCPBackend_Member/McpBackendMemberApplication.java
+++ src/main/java/AIWA/MCPBackend_Member/McpBackendMemberApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class McpBacendMemberApplication {
+public class McpBackendMemberApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(McpBacendMemberApplication.class, args);
+		SpringApplication.run(McpBackendMemberApplication.class, args);
 	}
 
 }
