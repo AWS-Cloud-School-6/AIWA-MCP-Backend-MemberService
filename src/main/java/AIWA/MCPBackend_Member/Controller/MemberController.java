3629169806f9ff3bac98149a87a85c08a0280977
gytjd <hys339631@gmail.com>
2024-10-30 18:21:30 +0900

    Update MemberController.java

--- src/main/java/AIWA/MCPBackend_Member/Controller/MemberController.java
+++ src/main/java/AIWA/MCPBackend_Member/Controller/MemberController.java
@@ -47,6 +47,9 @@ public class MemberController {
     @GetMapping("/email/{email}") // PathVariable로 이메일을 전달
     public SingleResult<MemberCredentialDTO> getMember(@PathVariable String email) {
         Member findMember = memberService.getMemberByEmail(email);  // Optional을 반환하지 않는다고 가정
+        System.out.println(email);
+        System.out.println(findMember.getAccess_key());
+        System.out.println(findMember.getSecret_key());
 
         if (findMember != null) {
             // Member 정보를 MemberCredentialDTO로 변환
