7c1731449540b99e6ed19285d63b4c3f3280bb49
nareunhang <xuwrgaf@gmail.com>
2024-10-30 11:43:04 +0900

    init: 초기 환경 설정

---
+++ src/main/java/AIWA/MCPBacend_Member/McpBacendMemberApplication.java
@@ -0,0 +1,13 @@
+package AIWA.MCPBacend_Member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpBacendMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpBacendMemberApplication.class, args);
+	}
+
+}
